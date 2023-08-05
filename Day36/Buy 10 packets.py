448fb1a27e23cb8c26a8ef2fa1ada327ae6bbf65
RAMKUMAR K <122148111+ramkumar-bitsathy@users.noreply.github.com>
2023-08-05 14:36:40 +0530

    Update Buy 10 packets.py

--- Day36/Buy 10 packets.py
+++ Day36/Buy 10 packets.py
@@ -1,1 +1,8 @@
-https://www.codechef.com/problems/TENPACKETS
+# cook your dish here
+T = int(input())
+for i in range(T):
+    X,Y = map(int,input().split())
+    if 2*X >= Y:
+        print(Y*2+X)
+    elif 2*X <Y:
+        print(X*5)
