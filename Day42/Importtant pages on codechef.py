1e8c8b85976aa830ae2e3dff6263a3911faeeefb
RAMKUMAR K <122148111+ramkumar-bitsathy@users.noreply.github.com>
2023-08-11 13:44:03 +0530

    Delete Importtant pages on codechef.py

--- Day42/Importtant pages on codechef.py
+++
@@ -1,7 +0,0 @@
-A, B = map(int, input().split())
-if A == 0:
-    print("https://www.codechef.com/practice")
-elif A == 1 and B == 0:
-    print("https://www.codechef.com/contests")
-else:
-    print("https://discuss.codechef.com")
