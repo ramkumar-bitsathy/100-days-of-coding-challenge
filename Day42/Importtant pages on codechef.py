9b13824f971a5874c95c4bd967ee3af8c187465f
RAMKUMAR K <122148111+ramkumar-bitsathy@users.noreply.github.com>
2023-08-11 13:43:34 +0530

    Create Importtant pages on codechef.py

---
+++ Day42/Importtant pages on codechef.py
@@ -0,0 +1,7 @@
+A, B = map(int, input().split())
+if A == 0:
+    print("https://www.codechef.com/practice")
+elif A == 1 and B == 0:
+    print("https://www.codechef.com/contests")
+else:
+    print("https://discuss.codechef.com")
