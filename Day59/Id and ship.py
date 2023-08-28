e4f0a7d87953effbbc117650509856dcefb58c51
RAMKUMAR K <122148111+ramkumar-bitsathy@users.noreply.github.com>
2023-08-28 19:57:19 +0530

    Create Id and ship.py

---
+++ Day59/Id and ship.py
@@ -0,0 +1,11 @@
+T = int(input())
+for i in range(T):
+    ID = input()
+    if ID == 'B' or ID == 'b':
+        print("BattleShip")
+    elif ID == 'C' or ID ==  'c':
+        print("Cruiser")
+    elif ID == 'D' or ID == 'd':
+        print("Destroyer")
+    elif ID == 'F' or ID == 'f':
+        print("Frigate")
