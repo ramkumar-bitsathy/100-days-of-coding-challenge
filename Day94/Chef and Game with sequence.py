97ea0fa23f0092ad66f09c4042f2b27d44ea01ce
RAMKUMAR K <122148111+ramkumar-bitsathy@users.noreply.github.com>
2023-10-03 21:15:41 +0530

    Create Chef and Game with sequence.py

---
+++ Day94/Chef and Game with sequence.py
@@ -0,0 +1,8 @@
+for T in range(int(input())):
+    n = int(input())
+    a = list(map(int,input().split()))
+    if n == 1:
+        print(1)
+        continue
+    odd = sum([1 for x in a if x%2])
+    print(odd%2 + 1)
