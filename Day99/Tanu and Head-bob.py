a44adedd00587df85eacbb51c1e768ed0458d91b
RAMKUMAR K <122148111+ramkumar-bitsathy@users.noreply.github.com>
2023-10-08 20:38:11 +0530

    Create Tanu and Head-bob.py

---
+++ Day99/Tanu and Head-bob.py
@@ -0,0 +1,9 @@
+for T in range(int(input())):
+    N = int(input())
+    S = input()
+    if 'I' in S :
+        print("INDIAN")
+    elif 'Y' in S:
+        print("NOT INDIAN")
+    else:
+        print("NOT SURE")
